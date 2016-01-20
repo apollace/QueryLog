e17b26ce79116826772fbc5d3fec88093e148766
apollace <alessandro.pollace@gmail.com>
2016-01-20 21:05:27 +0100

    Added comments required to understand the IQuery interface

--- source/src/org/polly/query/log/IQuery.java
+++ source/src/org/polly/query/log/IQuery.java
@@ -16,10 +16,44 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 package org.polly.query.log;
 
+/**
+ * The {@link IQuery} is the interface used to build a new query model. In this
+ * system the query contains the matching algorithm.
+ * 
+ * In the system must exists a reader, this reader read log files and than ask
+ * to query if the current line matches or not with the current user query.
+ * 
+ * The match maybe context sensitive, for this reason the sequence of log record
+ * must be respected.
+ * 
+ * This class is not thread safe.
+ * 
+ * @author Alessandro Pollace
+ */
 public interface IQuery {
+
+	/**
+	 * Store the user query.
+	 * 
+	 * @param query
+	 *            the query write by user
+	 * 
+	 * @return <code>true</code> if the query is a valid query,
+	 *         <code>false</code> otherwise.
+	 */
 	public boolean setQuery(String query);
+
+	/**
+	 * This method verify if the current line match with the user query and
+	 * previous log records.
+	 * 
+	 * @param line
+	 *            the current line of file;
+	 * 
+	 * @return <code>true</code> if the current line matches, <code>false</code>
+	 *         otherwise.
+	 */
 	public boolean match(String line);
 }
