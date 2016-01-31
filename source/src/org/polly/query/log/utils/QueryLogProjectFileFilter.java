bbf2ea50fe1058fdbdb901676c8b9492e487edb9
apollace <alessandro.pollace@gmail.com>
2016-01-31 18:00:20 +0100

    improved robustness of extension check

--- source/src/org/polly/query/log/utils/QueryLogProjectFileFilter.java
+++ source/src/org/polly/query/log/utils/QueryLogProjectFileFilter.java
@@ -1,3 +1,21 @@
+/**
+ * This file belonging to QueryLog an open source tool to search and trace
+ * information contained in your logs.  
+ * Copyright (C) 2016  Alessandro Pollace
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package org.polly.query.log.utils;
 
 import java.io.File;
@@ -7,12 +25,12 @@ public class QueryLogProjectFileFilter extends FileFilter {
 
 	@Override
 	public String getDescription() {
-		return "QueryLog project file";
+		return "QueryLog project file (qpl)";
 	}
 
 	@Override
 	public boolean accept(File f) {
-		return f.getName().endsWith(".qlp");
+		return f.getName().toLowerCase().endsWith(".qlp");
 	}
 
 }
